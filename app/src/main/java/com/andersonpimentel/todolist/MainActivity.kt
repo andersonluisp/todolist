7f4f33ad94a7fa93f99b1254fafc1cb6b3397ff1
andersonluisp <andersonluiscp@gmail.com>
2021-06-27 02:42:55 -0300

    create the recyclerview and item task layout

--- app/src/main/java/com/andersonpimentel/todolist/MainActivity.kt
+++ app/src/main/java/com/andersonpimentel/todolist/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.andersonpimentel.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.andersonpimentel.todolist.databinding.ActivityMainBinding
+import com.andersonpimentel.todolist.ui.AddTaskActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.fab.setOnClickListener {
+            startActivity(Intent(this, AddTaskActivity::class.java))
+        }
     }
 }
